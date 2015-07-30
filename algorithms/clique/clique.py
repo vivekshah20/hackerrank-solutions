40fd79cffe27e122352175f26a7cdfa647750bbd
Vivek Shah <vivek.shah93@gmail.com>
2015-07-30 19:49:14 +0530

    graph challenges

---
+++ algorithms/clique/clique.py
@@ -0,0 +1,18 @@
+def Turan(n, r):
+    return 0.5*(n**2-(n%r)*(n/r+1)**2-(r-(n%r))*(n/r)**2)
+
+
+t = input()
+for t in xrange(t):
+    N,M = map(int, raw_input().split(' '))
+    low,high=0,N
+    while low+1<high:
+        mid=(low+high)/2
+        r=Turan(N,mid)
+        
+        if r<M:
+            low=mid
+        else:
+            high=mid
+    
+    print high
